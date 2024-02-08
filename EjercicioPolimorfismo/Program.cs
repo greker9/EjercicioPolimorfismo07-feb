a0ca6895f91d92cec5a2ff5640d926c797041aa0
Greker9 <davidarturo9@gmail.com>
2024-02-07 19:35:24 -0600

    subiendo archivo con codigo

---
+++ EjercicioPolimorfismo/Program.cs
@@ -0,0 +1,35 @@
+﻿// Clase base o superclase
+public abstract class Animal
+{
+    public abstract void Sonido();
+}
+
+// Clase hija o subclase
+public class Perro : Animal
+{
+    public override void Sonido()
+    {
+        Console.WriteLine("El perro hace: guau guau");
+    }
+}
+
+// Clase hija o subclase
+public class Gato : Animal
+{
+    public override void Sonido()
+    {
+        Console.WriteLine("El gato hace: miau miau");
+    }
+}
+
+// Clase principal
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal miPerro = new Perro(); // Crear un objeto Perro
+        Animal miGato = new Gato(); // Crear un objeto Gato
+        miPerro.Sonido(); // El perro hace: guau guau
+        miGato.Sonido(); // El gato hace: miau miau
+    }
+}
